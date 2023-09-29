e08ae3f62b93b255e8f252ed31171e46c2b9db23
JaelsonJonas <livedojonas@hotmail.com>
2023-09-28 23:52:45 -0300

    Final

--- src/screens/Screen2.js
+++ src/screens/Screen2.js
@@ -1,44 +1,45 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 import { useValue } from './ValueContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
 const Screen2 = () => {
-  const { value, updateValue } = useValue();
-  const [newValue, setNewValue] = useState('');
+    const { value, updateValue } = useValue();
+    const [newValue, setNewValue] = useState('');
 
-  const navAsync = useNavAsync();
+    const navAsync = useNavigation();
 
-  const navigateToAsync = () => {
-      navigation.navigate('Screen2');
-  };
+    const navigateToAsync = () => {
+        navAsync.navigate('AsyncStorageComponent');
+    };
 
-  const saveNewValue = async () => {
-    try {
-      await AsyncStorage.setItem('value', newValue);
-      updateValue(newValue);
-      setNewValue('');
-    } catch (error) {
-      console.error('Erro ao salvar valor:', error);
-    }
-  };
+    const saveNewValue = async () => {
+        try {
+            await AsyncStorage.setItem('value', newValue);
+            updateValue(newValue);
+            setNewValue('');
+        } catch (error) {
+            console.error('Erro ao salvar valor:', error);
+        }
+    };
 
-  return (
-    <View>
-      <Text>Valor do Contexto na Tela 2: {value}</Text>
-      <TextInput
-        placeholder="Novo valor"
-        value={newValue}
-        onChangeText={(text) => setNewValue(text)}
-      />
-      <Button title="Salvar Novo Valor" onPress={saveNewValue} />
+    return (
+        <View>
+            <Text>Valor do Contexto na Tela 2: {value}</Text>
+            <TextInput
+                placeholder="Novo valor"
+                value={newValue}
+                onChangeText={(text) => setNewValue(text)}
+            />
+            <Button title="Salvar Novo Valor" onPress={saveNewValue} />
 
-      <TouchableOpacity
+            <TouchableOpacity
                 onPress={navigateToAsync} >
                 <Text>ir para Async Storege</Text>
             </TouchableOpacity>
-    </View>
-  );
+        </View>
+    );
 };
 
 export default Screen2;
